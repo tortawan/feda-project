a5ead371bb3450728a8a0ef7d48cbe5eb2955e92
tortawan <tortawan@hotmail.com>
2025-06-05 17:31:39 -0700

    modify setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='feda_project',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        # Add dependencies here, or leave blank if using requirements.txt
-    ],
+    version='1.0.0',
     author='Tawan Teopipithaporn',
-    description='Forest-guided Estimation of Distribution Algorithm (FEDA) project',
+    description='A Forest-guided Estimation of Distribution Algorithm (FEDA) for optimization.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/tortawan/feda-project', # Replace with your repo URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    # Core dependencies needed for your project to run
+    install_requires=[
+        'numpy>=1.21.0',
+        'scikit-learn>=1.0.0',
+        'matplotlib>=3.0.0',
+    ],
+    # Extra dependencies for development and testing
+    extras_require={
+        'test': ['pytest>=7.0.0'],
+    },
 )
